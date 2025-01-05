61fafdb68cbce266bffe13ad7a8c221e3bdf7af2
nullity00 <reshmashaik3011@gmail.com>
2025-01-05 18:31:16 +0400

    back to top, code black styling

--- pages/reports/[slug].tsx
+++ pages/reports/[slug].tsx
@@ -21,10 +21,17 @@ export default function ReportPage({
   return (
     <main className="bg-gray-50 flex flex-col min-h-screen">
       <div className=" bg-gray-50 mt-10 mx-auto">
-        <Link href="/"><h2 className="text-xl mb-4 text-black">← Back to Reports</h2></Link>
+        <Link href="/">
+          <h2 className="text-xl mb-4 text-black">← Back to Reports</h2>
+        </Link>
 
         <div className="max-w-7xl bg-white shadow py-6 sm:px-6 flex flex-row justify-between ">
-          <h1 className="text-3xl font-bold text-black">{title}</h1>
+          <h1
+            className="text-3xl font-bold text-black"
+            id={title.toLocaleLowerCase()}
+          >
+            {title}
+          </h1>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             {/* <span>By {author}</span>
             <span className="mx-2">•</span> */}
@@ -39,7 +46,7 @@ export default function ReportPage({
             {tags.map((tag, index) => (
               <span
                 key={index}
-                className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emeraldlight bg-opacity-25 text-darkgreen"
+                className="inline-flex items-center px-3 py-1.5 rounded-full text-md font-medium bg-emeraldlight bg-opacity-25 text-darkgreen"
               >
                 {tag}
               </span>
@@ -51,6 +58,11 @@ export default function ReportPage({
             className="prose prose-lg max-w-none prose-table:shadow-lg prose-table:border prose-td:p-2 prose-th:p-2 prose-th:bg-gray-100"
             dangerouslySetInnerHTML={{ __html: content }}
           />
+        </div>
+        <div className="py-8">
+          <Link href={"#" + title.toLocaleLowerCase()}>
+            <h2 className="text-xl mb-4 text-black">↑ Back to Top</h2>
+          </Link>
         </div>
       </div>
     </main>
