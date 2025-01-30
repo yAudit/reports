e8014692f08567b9177d8ade76777e299c4a9a0b
nullity00 <reshmashaik3011@gmail.com>
2025-01-30 20:12:54 +0400

    new window for reports

--- components/ReportCard.tsx
+++ components/ReportCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 interface ReportCardProps {
   title: string;
@@ -26,34 +25,31 @@ const ReportCard: React.FC<ReportCardProps> = ({
     });
   };
 
-  const router = useRouter();
-
   return (
     <div
       className="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200 overflow-hidden hover:cursor-pointer duration-700"
-      onClick={() => router.push(`/reports/${slug}`)}
     >
-      <div className="p-6">
-        <div className="flex items-center justify-between mb-2">
-          <Link href={`/reports/${slug}`}>
+      <Link href={`/reports/${slug}`}>
+        <div className="p-6">
+          <div className="flex items-center justify-between mb-2">
             <h2 className="text-xl font-semibold text-black">{title}</h2>
-          </Link>
-        </div>
-        <p className="text-gray-600 mb-4">{description}</p>
-        <div className="flex items-center justify-between">
-          <div className="flex flex-wrap gap-2">
-            {tags.map((tag, index) => (
-              <span
-                key={index}
-                className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emeraldlight bg-opacity-25 text-darkgreen"
-              >
-                {tag}
-              </span>
-            ))}
           </div>
-          <p className="text-sm text-gray-500">{formatDate(date)}</p>
+          <p className="text-gray-600 mb-4">{description}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-wrap gap-2">
+              {tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emeraldlight bg-opacity-25 text-darkgreen"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <p className="text-sm text-gray-500">{formatDate(date)}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
